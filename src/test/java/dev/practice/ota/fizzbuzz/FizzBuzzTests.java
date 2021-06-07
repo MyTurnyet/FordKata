3462bcc5363c9fe912cb91b21aff0c45a34383af
PWATSO68 <pwatson68@ford.com>
2021-06-07 06:42:42 -0700

    Initial commit

---
+++ src/test/java/dev/practice/ota/fizzbuzz/FizzBuzzTests.java
@@ -0,0 +1,14 @@
+package dev.practice.ota.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzTests
+{
+    @Test
+    public void should_fail()
+    {
+        assertThat(true).isFalse();
+    }
+}
