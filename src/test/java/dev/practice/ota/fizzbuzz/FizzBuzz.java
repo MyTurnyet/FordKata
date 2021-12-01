252a907fa18f1d7b38ff9db460e8ae7cde99fb0f
Paige Watson <paige.watson@outlook.com>
2021-12-01 12:35:48 -0800

    "mob next [ci-skip]"

---
+++ src/test/java/dev/practice/ota/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,13 @@
+package dev.practice.ota.fizzbuzz;
+
+public class FizzBuzz
+{
+    public FizzBuzz(int numberToConvert)
+    {
+    }
+
+    public String convert()
+    {
+        return null;
+    }
+}
--- src/test/java/dev/practice/ota/fizzbuzz/FizzBuzzTests.java
+++ src/test/java/dev/practice/ota/fizzbuzz/FizzBuzzTests.java
@@ -4,11 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/****
+ * Given a number 1-100
+ * if divisible by 3 return "fizz"
+ * if divisible by 5 return "buzz"
+ * if divisible by 3 & 5 return "fizzbuzz"
+ *
+ * else return number as a string (ie: 1=="1")
+ *
+ */
+
 public class FizzBuzzTests
 {
     @Test
-    public void should_fail()
+    public void should_return1_whenPassed1()
     {
-        assertThat(true).isFalse();
+        final FizzBuzz fizzbuzz = new FizzBuzz(1);
+        assertThat(fizzbuzz.convert()).isEqualTo("1");
     }
+
 }
